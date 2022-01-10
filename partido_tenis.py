7b9ea0f7c8739d2b3a193b530d4679644ea42602
ciamassih <c.abdulmassihlopez1@usp.ceu.es>
2022-01-10 17:33:19 +0100

    test inicio partido player 1 OK

--- partido_tenis.py
+++ partido_tenis.py
@@ -1,3 +1,4 @@
-class Tenis:
-    def score(self):
-        return "inicio de partido"
+class Score:
+
+    def __init__(self, player1):
+        self.player1 = 0
--- tests_tenis.py
+++ tests_tenis.py
@@ -1,9 +1,9 @@
 import unittest
 
-from partido_tenis import Tenis
+from partido_tenis import Score
 
 class PartidoTenis(unittest.TestCase):
 
     def test_inicio_partido(self):
-        tenis = Tenis()
-        self.assertEqual("inicio de partido", tenis.score())
+        score = Score(0)
+        self.assertEqual(score.player1, 0)
