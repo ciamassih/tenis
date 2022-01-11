a118bba322021f9fd5d6729b4dce3dbd13d74ce3
ciamassih <c.abdulmassihlopez1@usp.ceu.es>
2022-01-11 14:52:12 +0100

    puntaje de 15 a 30 fallando

--- partido_tenis.py
+++ partido_tenis.py
@@ -3,3 +3,5 @@ class Score:
     def __init__(self, puntos):
         self.puntos = puntos
 
+    def anotacion(self, cantidad):
+        pass
--- tests_tenis.py
+++ tests_tenis.py
@@ -11,3 +11,14 @@ class PartidoTenis(unittest.TestCase):
     def test_puntaje_en_15(self):
         score1 = Score(15)
         self.assertEqual(score1.puntos, 15)
+
+    def test_puntaje_en_0_con_los_2_jugadores(self):
+        score1 = Score(0)
+        score2 = Score(0)
+        self.assertEqual(score1.puntos, 0)
+        self.assertEqual(score2.puntos, 0)
+
+    def test_puntaje_en_15_a_30(self):
+        score1 = Score(15)
+        score1.anotacion(15)
+        self.assertEqual(score1.puntos, 30)
