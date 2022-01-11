2a962361104cabec40c7a76326ebfcc22edd0d88
ciamassih <c.abdulmassihlopez1@usp.ceu.es>
2022-01-11 15:28:47 +0100

    test ganar set OK

--- partido_tenis.py
+++ partido_tenis.py
@@ -3,12 +3,14 @@ class Score:
     def __init__(self, puntos):
         self.puntos = puntos
 
-    def anotacion(self, score1):
-        if (score1.puntos >= 0) and (score1.puntos <= 29):
+    def anotacion(self, score):
+        if (score.puntos >= 0) and (score.puntos <= 29):
             self.puntos += 15
-        elif (score1.puntos >= 30):
+        elif (score.puntos >= 30):
             self.puntos += 10
 
-    def ganarset(self, score1):
-        if (score1.puntos >= 40):
-            self.puntos = "Set ganado"
+    def ganarset(self, score1, score2):
+        if (score1.puntos >= 40) and (score2.puntos <= 30):
+            self.puntos = "Set ganado por el jugador 1"
+        elif (score2.puntos >= 40) and (score1.puntos <= 30):
+            self.puntos = "Set ganado por el jugador 2"
--- tests_tenis.py
+++ tests_tenis.py
@@ -22,14 +22,28 @@ class PartidoTenis(unittest.TestCase):
         score1.anotacion(score1)
         self.assertEqual(score1.puntos, 40)
 
-    def test_punto_ganador(self):
+    def test_punto_ganador_por_jugador_1(self):
         score1 = Score(40)
-        score1.ganarset(score1)
-        self.assertEqual(score1.puntos, "Set ganado")
+        score2 = Score(15)
+        score1.ganarset(score1, score2)
+        self.assertEqual(score1.puntos, "Set ganado por el jugador 1")
 
-    def test_puntaje_en_0_primer_punto_jugador_1(self):
+    def test_punto_ganador_por_jugador_2(self):
+        score1 = Score(30)
+        score2 = Score(40)
+        score2.ganarset(score1, score2)
+        self.assertEqual(score2.puntos, "Set ganado por el jugador 2")
+
+    def test_puntaje_en_0_primer_punto_jugador_2(self):
         score1 = Score(0)
         score2 = Score(0)
-        score1.anotacion(score1)
-        self.assertEqual(score1.puntos, 15)
-        self.assertEqual(score2.puntos, 0)
+        score2.anotacion(score2)
+        self.assertEqual(score1.puntos, 0)
+        self.assertEqual(score2.puntos, 15)
+
+    def test_puntaje_40_40(self):
+        score1 = Score(0)
+        score2 = Score(0)
+        score2.anotacion(score2)
+        self.assertEqual(score1.puntos, 0)
+        self.assertEqual(score2.puntos, 15)
