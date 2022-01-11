3cde20e93db472f01a86365528063a33857e01ab
ciamassih <c.abdulmassihlopez1@usp.ceu.es>
2022-01-11 14:47:00 +0100

    puntaje en 15 fallando

--- tests_tenis.py
+++ tests_tenis.py
@@ -7,3 +7,7 @@ class PartidoTenis(unittest.TestCase):
     def test_puntaje_en_0(self):
         score1 = Score(0)
         self.assertEqual(score1.puntos, 0)
+
+    def test_puntaje_en_15(self):
+        score1 = Score(15)
+        self.assertEqual(score1.puntos, 15)
